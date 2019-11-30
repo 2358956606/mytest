270e911dcd016b97348c8f9c3a4c8d9e4817fd12
Administrator <2358956606@qq.com>
2019-11-30 17:43:13 +0800

    修改后第二次推送工程

--- test/src/main/java/user.java
+++ test/src/main/java/user.java
@@ -6,6 +6,12 @@ public class user {
         System.out.println("你好");
         System.out.println("你好");
         System.out.println("你好");
+        //第二次使用git上传
     }
 
+    public void test(){
+        int i =0;
+        i+=10;
+        System.out.println(i);
+    }
 }
