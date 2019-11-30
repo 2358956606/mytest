e2bb7224bacf43db23e7e2cd3a1f89369d05ee12
Administrator <2358956606@qq.com>
2019-11-30 17:28:53 +0800

    使用idea上传文件测试

---
+++ test/src/main/java/user.java
@@ -0,0 +1,11 @@
+public class user {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+    }
+
+}
