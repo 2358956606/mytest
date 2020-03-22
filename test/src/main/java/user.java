5e912ac639a9b81a28db6c914452e50d4dac0ed5
Administrator <2358956606@qq.com>
2020-03-22 17:53:04 +0800

    修改了请求

--- test/src/main/java/user.java
+++ test/src/main/java/user.java
@@ -3,15 +3,7 @@ public class user {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        //第二次使用git上传
-    }
+           }
 
-    public void test(){
-        int i =0;
-        i+=10;
-        System.out.println(i);
-    }
+
 }
